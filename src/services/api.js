0732f98078b46b348587eed05675debb5e92bad1
Ulisses Marciano Gonçalves <ulissesmkt2104@gmail.com>
2024-07-06 15:15:56 -0300

    Adicionando token para extensão da renderização do api

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,13 @@
 import axios from "axios";
 
+const token = process.env.REACT_APP_GITHUB_TOKEN;
+
 const api = axios.create({
     baseURL: "https://api.github.com/users/",
+
+    headers: {
+        Authorization: `token ${token}`,
+    },
 });
 
 export default api;
