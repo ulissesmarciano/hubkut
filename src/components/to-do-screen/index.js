4ff4a3fa5d9f1edb81a1cff18a67836449ada1a7
Ulisses Marciano Gonçalves <ulissesmkt2104@gmail.com>
2024-07-13 22:50:50 -0300

    Criação do mockup para a todo list screen

--- src/components/to-do-screen/index.js
+++ src/components/to-do-screen/index.js
@@ -1,10 +1,17 @@
-import React from 'react'
-import { Container } from './styles'
+import React from 'react';
+import { Container } from './styles';
+import LinkItem from '../link';
 
 export default function ToDoScreen() {
   return (
     <Container>
-      ToDoScreen
+      <h4>O que você deseja fazer?</h4>
+      <div className='link-container'>
+        <LinkItem variant="todoLinkPrimary" name="Criar repositório"/>
+        <LinkItem variant="todoLinkSecondary" name="Clonar repositório"/>
+        <LinkItem variant="todoLinkSecondary" name="Suas estrelas"/>
+        <LinkItem variant="todoLinkSecondary" name="Novo Gist"/>
+      </div>
     </Container>
-  )
-}
+  );
+};
--- src/components/to-do-screen/styles.js
+++ src/components/to-do-screen/styles.js
@@ -1,6 +1,17 @@
 import styled from "styled-components";
 
 export const Container = styled.section`
+    padding: 1.4rem 1rem;
+
     display: flex;
+    flex-direction: column;
     background-color: #fff;
+    border-radius: .3rem;
+
+    gap: 1rem;
+
+    .link-container {
+        display: flex;
+        gap: 1rem;
+    }
 `
