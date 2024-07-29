f34851ac6bbdfcbf67a1dcdf4f6df2fd174b309d
Ulisses Marciano Gonçalves <ulissesmkt2104@gmail.com>
2024-07-29 15:18:56 -0300

    Atualização e refatoração de código

--- src/components/to-do-screen/index.js
+++ src/components/to-do-screen/index.js
@@ -7,10 +7,29 @@ export default function ToDoScreen({username}) {
     <Container>
       <h4>O que você deseja fazer?</h4>
       <div className='link-container'>
-        <LinkItem variant="todoLinkPrimary" name="Criar repositório" to="https://github.com/new" target="_blank"/>
-        <LinkItem variant="todoLinkSecondary" name="Configurações" to="https://github.com/settings/profile" target="_blank"/>
-        <LinkItem variant="todoLinkSecondary" name="Suas estrelas" to={`https://github.com/${username}?tab=stars`} target="_blank"/>
-        <LinkItem variant="todoLinkSecondary" name="Novo Gist" to="https://gist.github.com/" target="_blank"/>
+        <LinkItem 
+          variant="todoLinkPrimary"
+          name="Criar repositório" 
+          to="https://github.com/new" 
+          target="_blank"
+        />
+        <LinkItem 
+          variant="todoLinkSecondary" 
+          name="Configurações" 
+          to="https://github.com/settings/profile" target="_blank"
+        />
+        <LinkItem 
+          variant="todoLinkSecondary" 
+          name="Suas estrelas" 
+          to={`https://github.com/${username}?tab=stars`} 
+          target="_blank"
+        />
+        <LinkItem 
+          variant="todoLinkSecondary" 
+          name="Novo Gist" 
+          to="https://gist.github.com/" 
+          target="_blank"
+        />
       </div>
     </Container>
   );
