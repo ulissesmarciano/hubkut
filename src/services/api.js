07d24b61120723e217c1b0a8bf24755b0410fecb
Ulisses Marciano Gonçalves <ulissesmkt2104@gmail.com>
2024-08-01 20:07:17 -0300

    Atualização da aplicação do token para funcionamento no vercel

--- src/services/api.js
+++ src/services/api.js
@@ -1,12 +1,11 @@
 import axios from "axios";
 
-const token = process.env.REACT_APP_GITHUB_TOKEN;
 
 const api = axios.create({
     baseURL: "https://api.github.com/users/",
 
     headers: {
-        Authorization: `token ${token}`,
+        Authorization: `Bearer ${process.env.REACT_APP_GITHUB_TOKEN}`,
     },
 });
 
