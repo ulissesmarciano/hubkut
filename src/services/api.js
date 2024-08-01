d276d2d865ef0bcaaa5d47701e028a3873f02a23
Ulisses Marciano Gonçalves <ulissesmkt2104@gmail.com>
2024-08-01 20:29:39 -0300

    Adicionando Autorização de acesso para o vercel

--- src/services/api.js
+++ src/services/api.js
@@ -1,10 +1,12 @@
 import axios from "axios";
 
+//const token = process.env.REACT_APP_GITHUB_TOKEN; //adicionar para renderização local
 
 const api = axios.create({
     baseURL: "https://api.github.com/users/",
 
     headers: {
+        //Authorization: `token ${token}`, //adicionar para renderização local
         Authorization: `Bearer ${process.env.REACT_APP_GITHUB_TOKEN}`,
     },
 });
