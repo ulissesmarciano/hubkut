0adeae2f24555413bc01c7d694c589b89d910e9b
Ulisses <ulissesmkt2104@gmail.com>
2024-09-13 13:59:19 -0300

    Adição de novos parametros para melhor funcionamento do sidebar no header

--- src/components/link/index.jsx
+++ src/components/link/index.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import { Container } from './styles';
 
-export default function LinkItem({variant, name, to, target}) {
+const LinkItem = ({variant, name, to, target, onClick}) => {
   return (
-    <Container to={to} variant={variant} target={target}>
+    <Container to={to} variant={variant} target={target} onClick={onClick}>
         {name}
     </Container>
   );
 };
+
+export default LinkItem;
