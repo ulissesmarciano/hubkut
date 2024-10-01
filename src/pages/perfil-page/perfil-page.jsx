a16ed7c6b8a6ef5046daf7dea73419e42568b778
Ulisses <ulissesmkt2104@gmail.com>
2024-10-01 16:02:42 -0300

    Atualização: Formatação de página

--- src/pages/perfil-page/perfil-page.jsx
+++ src/pages/perfil-page/perfil-page.jsx
@@ -15,9 +15,9 @@ import useFetchFollowersData from '../../hooks/useFetchFollowersData';
 import useFetchFollowingData from '../../hooks/useFetchFollowingData';
 import useFetchReposData from '../../hooks/useFetchReposData';
 
-export default function PerfilPage () {
+export default function PerfilPage() {
   const [loading, setLoading] = useState(true);
-  const {username} = useParams();
+  const { username } = useParams();
   const userData = useFetchUserData(username);
   const followingData = useFetchFollowingData(username);
   const followersData = useFetchFollowersData(username);
@@ -29,72 +29,72 @@ export default function PerfilPage () {
       followingData &&
       followersData &&
       reposData
-    ){
+    ) {
       setLoading(false);
     }
-  },[userData, followingData, followersData, reposData]);
-  
+  }, [userData, followingData, followersData, reposData]);
+
   return (
     <>
-      <Header 
+      <Header
         followersLinkHref={username}
         followingLinkHref={username}
       />
       {loading ? (
         <Loader />
-      ):(
+      ) : (
         <Container>
-        <aside className='user-side'>
-          <UserScreen
-            src={userData.avatar_url}
-            alt={`foto de ${userData.name}`}
-            name={userData.name}
-            employ={userData.company}
-            city={userData.location}
-            email={userData.email}
-            portfolio={userData.blog}
-            portfolioHref={userData.blog}
-            xUsername={userData.twitter_username}
-            xHref={userData.twitter_username}
-          />
-        </aside>
-        <section className='info-section'>
-          <ReposScreen
-            repoPageLink={`/repos/${userData.login}`}
-            repoItem={reposData.map((repo, index) => 
-              <RepoItem 
-                key={index}
-                repoName={repo.name}
-                repoUrl={repo.full_name}
-                to={repo.svn_url}
-              />
-            ).slice(0, 5)}
-          />
-        </section>
-        <aside className='follow-side'>
-          <section>
-            <FollowSection
+          <aside className='user-side'>
+            <UserScreen
+              src={userData.avatar_url}
+              alt={`foto de ${userData.name}`}
+              name={userData.name}
+              employ={userData.company}
+              city={userData.location}
+              email={userData.email}
+              portfolio={userData.blog}
+              portfolioHref={userData.blog}
+              xUsername={userData.twitter_username}
+              xHref={userData.twitter_username}
+            />
+          </aside>
+          <section className='info-section'>
+            <ReposScreen
+              repoPageLink={`/repos/${userData.login}`}
+              repoItem={reposData.map((repo, index) =>
+                <RepoItem
+                  key={index}
+                  repoName={repo.name}
+                  repoUrl={repo.full_name}
+                  to={repo.svn_url}
+                />
+              ).slice(0, 5)}
+            />
+          </section>
+          <aside className='follow-side'>
+            <section>
+              <FollowSection
                 typeName="seguindo"
                 count={userData.following}
                 to={`/following/${username}`}
                 followUser={(followingData.map((user, index) =>
                   <FollowUser
-                    key={index} 
+                    key={index}
                     imageUrl={user.avatar_url}
                     usernameIn={user.login}
                     variant="secondary"
                     to={user.login}
                   />
                 ).slice(0, 6))}
-            />
-          </section>
-          <section>
-            <FollowSection
+              />
+            </section>
+            <section>
+              <FollowSection
                 typeName="seguidores"
                 count={userData.followers}
                 followUser={(followersData.map((user, index) =>
                   <FollowUser
-                    key={index} 
+                    key={index}
                     imageUrl={user.avatar_url}
                     usernameIn={user.login}
                     variant="secondary"
@@ -102,10 +102,10 @@ export default function PerfilPage () {
                   />
                 ).slice(0, 6))}
                 to={`/followers/${username}`}
-            />
-          </section>
-        </aside>
-      </Container>
+              />
+            </section>
+          </aside>
+        </Container>
       )}
     </>
   );
