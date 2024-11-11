a43c7e7bff56016e8c37558f2b218e99b64d7943
Ulisses <ulissesmkt2104@gmail.com>
2024-11-10 23:01:44 -0300

    Atualização: Realizando todas as importações

--- src/hooks/useUserData.js
+++ src/hooks/useUserData.js
@@ -1,6 +1,14 @@
+import { useState, useEffect } from "react";
+import useFetchUserData from "./useFetchUserData";
+import useFetchReposData from "./useFetchReposData";
+import useFetchStarredData from "./useFetchStarredData";
+import useFetchFollowersData  from "./useFetchFollowersData ";
+import useFetchFollowingData from "./useFetchFollowingData";
+
+
 const user = {
     name: '',
     username: '',
     picture: '',
     
-}
+};
