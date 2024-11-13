5fe1c0b65f88ee7d76fd5be399b21c7b54cc694d
Ulisses <ulissesmkt2104@gmail.com>
2024-11-12 23:28:25 -0300

    Adicionando novos itens ao objeto criado

--- src/hooks/useUserData.js
+++ src/hooks/useUserData.js
@@ -2,13 +2,34 @@ import { useState, useEffect } from "react";
 import useFetchUserData from "./useFetchUserData";
 import useFetchReposData from "./useFetchReposData";
 import useFetchStarredData from "./useFetchStarredData";
-import useFetchFollowersData  from "./useFetchFollowersData ";
+import useFetchFollowersData from "./useFetchFollowersData";
 import useFetchFollowingData from "./useFetchFollowingData";
 
 
-const user = {
-    name: '',
-    username: '',
-    picture: '',
+const useUserData = (username) => {
+    const [user, setUser] = useState({
+        name: '',
+        username: '',
+        picture: '',
+    })
+
+    const userData = useFetchUserData(username);
+    const reposData = useFetchReposData(username);
+    const starredData = useFetchStarredData(username);
+    const followersData = useFetchFollowersData(username);
+    const followingData = useFetchFollowingData(username);
+
+    useEffect(() => {
+        if (userData) {
+            setUser(prevState => ({
+                ...prevState,
+                
+            }))
+        }
+    }, [userData])
+
+    return user
     
 };
+
+export default useUserData;
