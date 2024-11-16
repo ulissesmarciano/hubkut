d5819f4bdbe0c526d0427814ec349ce6a3399f87
Ulisses <ulissesmkt2104@gmail.com>
2024-11-15 22:14:55 -0300

    Atualização: Criando lógica para fazer aparecer o item de lista somente quando houver

--- src/components/user-screen/index.jsx
+++ src/components/user-screen/index.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Container } from './styles';
-import { Link } from 'react-router-dom';
 
 import EmployEmoji from '../../assets/icons/company-icon.png';
 import PinIcon from '../../assets/icons/pin-icon.png';
@@ -8,7 +7,6 @@ import MailIcon from '../../assets/icons/mail-icon.png';
 import LinkIcon from '../../assets/icons/link-icon.png';
 import XIcon from '../../assets/icons/x_icon.svg';
 import InfoListItem from '../info-list-item';
-import { MdMarkEmailRead } from 'react-icons/md';
 
 
 export default function UserScreen({
@@ -26,44 +24,40 @@ export default function UserScreen({
 }) {
   return (
     <Container>
-      	<picture className='user-profile-picture'>
-          <img src={src} alt={alt} />
-        </picture>
-        <span />
-        <div className='username-section'>
-          <h3>{name}</h3>
-          <p>{profession}</p>
-        </div>
-        <span />
-        <ul className='info-list-section'>
-          <InfoListItem 
-            itemEmoji={EmployEmoji}
-            itemName={employ}
-          />
-          <InfoListItem 
-            itemEmoji={PinIcon}
-            itemName={city}
-          />
-          <InfoListItem 
-            itemEmoji={MailIcon}
-            itemName={email}
-          />
-          <InfoListItem 
-            itemEmoji={LinkIcon}
-            itemName={employ}
-          />
-          <InfoListItem 
-            itemEmoji={EmployEmoji}
-            itemName={portfolio}
-            to={portfolioHref}
-          />
-          <InfoListItem 
-            itemEmoji={XIcon}
-            itemName={xUsername}
-            to={`https://x.com/${xHref}`} 
-          />
-        </ul>
-        <span />
+      <picture className='user-profile-picture'>
+        <img src={src} alt={alt} />
+      </picture>
+      <span />
+      <div className='username-section'>
+        <h3>{name}</h3>
+        <p>{profession}</p>
+      </div>
+      <span />
+      <ul className='info-list-section'>
+        {employ && (<InfoListItem
+          itemEmoji={EmployEmoji}
+          itemName={employ}
+        />)}
+        {city && (<InfoListItem
+          itemEmoji={PinIcon}
+          itemName={city}
+        />)}
+        {email && (<InfoListItem
+          itemEmoji={MailIcon}
+          itemName={email}
+        />)}
+        {portfolio && (<InfoListItem
+          itemEmoji={LinkIcon}
+          itemName={portfolio}
+          to={portfolioHref}
+        />)}
+        {xUsername && (<InfoListItem
+          itemEmoji={XIcon}
+          itemName={xUsername}
+          to={`https://x.com/${xHref}`}
+        />)}
+      </ul>
+      <span />
     </Container>
   );
 };
