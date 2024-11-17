4f6df8a9b118b46018eb3076b6ef092f7156806d
Ulisses <ulissesmkt2104@gmail.com>
2024-11-17 12:02:21 -0300

    Atualização: Criação de novos parametros para redirecionamento de página

--- src/components/info-list-item/index.jsx
+++ src/components/info-list-item/index.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-const InfoListItem = ({ itemEmoji, itemName, to }) => {
+const InfoListItem = ({ itemEmoji, itemName, to, target }) => {
     return (
         <li>
             <img src={itemEmoji} alt='ícone empresa' />
             {to ? (
-                <Link to={to}>
+                <Link to={to} target={target}>
                     <p>{itemName}</p>
                 </Link>) : (
                 <p>{itemName}</p>
