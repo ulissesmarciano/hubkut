364e2874f1828dce574ad715aadb3a95051af325
Ulisses <ulissesmarcianodev@gmail.com>
2025-01-21 10:59:23 -0300

    Atualização: Formatação dos componentes no diretório src

--- src/App.js
+++ src/App.js
@@ -1,30 +1,30 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import { UserProvider } from './context/UserContext';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { UserProvider } from "./context/UserContext";
 
-import LoginPage from './pages/loginpage/loginpage';
-import HomePage from './pages/homepage/homepage';
-import FollowingPage from './pages/following-page/following-page';
-import FollowersPage from './pages/followers-page/followers-page';
-import ReposPage from './pages/repos-page/repos-page';
-import StarredPage from './pages/starred-page/starred-page';
-import PerfilPage from './pages/perfil-page/perfil-page';
+import LoginPage from "./pages/loginpage/loginpage";
+import HomePage from "./pages/homepage/homepage";
+import FollowingPage from "./pages/following-page/following-page";
+import FollowersPage from "./pages/followers-page/followers-page";
+import ReposPage from "./pages/repos-page/repos-page";
+import StarredPage from "./pages/starred-page/starred-page";
+import PerfilPage from "./pages/perfil-page/perfil-page";
 
 function App() {
   return (
     <UserProvider>
       <Router>
         <Routes>
-            <Route path="/" element={<LoginPage/>} />
-            <Route path="/home/:username" element={<HomePage />}/>
-            <Route path="/following/:username" element={<FollowingPage />} />
-            <Route path="/followers/:username" element={<FollowersPage/>} />
-            <Route path="/repos/:username" element={<ReposPage />} />
-            <Route path="/starred/:username" element={<StarredPage />} />
-            <Route path="/perfil/:username" element={<PerfilPage />} />
+          <Route path="/" element={<LoginPage />} />
+          <Route path="/home/:username" element={<HomePage />} />
+          <Route path="/following/:username" element={<FollowingPage />} />
+          <Route path="/followers/:username" element={<FollowersPage />} />
+          <Route path="/repos/:username" element={<ReposPage />} />
+          <Route path="/starred/:username" element={<StarredPage />} />
+          <Route path="/perfil/:username" element={<PerfilPage />} />
         </Routes>
       </Router>
     </UserProvider>
   );
-};
+}
 
 export default App;
