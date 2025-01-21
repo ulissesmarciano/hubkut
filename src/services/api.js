eebde484ff3c309cd0f7b65de7a4b8c109957e8b
Ulisses <ulissesmarcianodev@gmail.com>
2025-01-21 10:57:05 -0300

    Atualização: Formatação dos componentes no diretório services

--- src/services/api.js
+++ src/services/api.js
@@ -3,12 +3,12 @@ import axios from "axios";
 //const token = process.env.REACT_APP_GITHUB_TOKEN; //adicionar para renderização local
 
 const api = axios.create({
-    baseURL: "https://api.github.com/users/",
+  baseURL: "https://api.github.com/users/",
 
-    headers: {
-        //Authorization: `token ${token}`, //adicionar para renderização local
-        Authorization: `Bearer ${process.env.REACT_APP_GITHUB_TOKEN}`,
-    },
+  headers: {
+    //Authorization: `token ${token}`, //adicionar para renderização local
+    Authorization: `Bearer ${process.env.REACT_APP_GITHUB_TOKEN}`,
+  },
 });
 
 export default api;
