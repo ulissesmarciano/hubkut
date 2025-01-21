b6aec028851519069e6e0d975b425da4afcde803
Ulisses <ulissesmarcianodev@gmail.com>
2025-01-21 11:05:38 -0300

    Atualização: Correção de rotas no App

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { UserProvider } from "./context/UserContext";
 
-import LoginPage from "./pages/loginpage/loginpage";
-import HomePage from "./pages/homepage/homepage";
-import FollowingPage from "./pages/following-page/following-page";
-import FollowersPage from "./pages/followers-page/followers-page";
-import ReposPage from "./pages/repos-page/repos-page";
-import StarredPage from "./pages/starred-page/starred-page";
-import PerfilPage from "./pages/perfil-page/perfil-page";
+import LoginScreen from "./pages/LoginScreen/LoginScreen"
+import HomeScreen from "./pages/HomeScreen/HomeScreen";
+import FollowingScreen from "./pages/FollowingScreen/FollowingScreen"
+import FollowersScreen from "./pages/FollowersScreen/FollowerScreen";
+import ReposScreen from "./pages/ReposScreen/ReposScreen";
+import StarredScreen from "./pages/StarredScreen/StarredScreen";
+import PerfilScreen from "./pages/PerfilScreen/PerfilScreen";
 
 function App() {
   return (
     <UserProvider>
       <Router>
         <Routes>
-          <Route path="/" element={<LoginPage />} />
-          <Route path="/home/:username" element={<HomePage />} />
-          <Route path="/following/:username" element={<FollowingPage />} />
-          <Route path="/followers/:username" element={<FollowersPage />} />
-          <Route path="/repos/:username" element={<ReposPage />} />
-          <Route path="/starred/:username" element={<StarredPage />} />
-          <Route path="/perfil/:username" element={<PerfilPage />} />
+          <Route path="/" element={<LoginScreen />} />
+          <Route path="/home/:username" element={<HomeScreen />} />
+          <Route path="/following/:username" element={<FollowingScreen />} />
+          <Route path="/followers/:username" element={<FollowersScreen />} />
+          <Route path="/repos/:username" element={<ReposScreen />} />
+          <Route path="/starred/:username" element={<StarredScreen />} />
+          <Route path="/perfil/:username" element={<PerfilScreen />} />
         </Routes>
       </Router>
     </UserProvider>
