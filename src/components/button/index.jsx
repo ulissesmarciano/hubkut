50b152f5e63d566e12dc730de7e9820a0ca0b457
Ulisses <ulissesmarcianodev@gmail.com>
2025-01-22 17:14:04 -0300

    Atualização: Mudando o escopo de função

--- src/components/button/index.jsx
+++ src/components/button/index.jsx
@@ -1,12 +1,10 @@
 import React from "react";
 import { Container } from "./styles";
 
-const Button = ({ type, onClick, variant }) => {
+export default function Button({ type, onClick, variant }) {
   return (
     <Container type={type} onClick={onClick} $variant={variant}>
       Entrar
     </Container>
   );
-};
-
-export default Button;
+}
