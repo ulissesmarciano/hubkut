3102d750383977ac492a6786305b27a773630254
Ulisses <ulissesmarcianodev@gmail.com>
2025-01-27 22:20:58 -0300

    Atualização: Mudança na variável do token para compatibilidade no vercel

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const githubToken = process.env.GITHUB_TOKEN;
+const githubToken = process.env.NEXT_PUBLIC_GITHUB_TOKEN;
 
 const api = axios.create({
   baseURL: "https://api.github.com/users/",
